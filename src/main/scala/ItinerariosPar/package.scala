bac79af034c70e2d4c1c27775e5dd3f6442b4f36
BayronJDv <135305164+BayronJDv@users.noreply.github.com>
2024-05-26 10:56:09 -0500

    Update package.scala
    implementacion funcion itinerariospar

--- src/main/scala/ItinerariosPar/package.scala
+++ src/main/scala/ItinerariosPar/package.scala
@@ -1,3 +1,48 @@
-package object ItinerariosPar {
+import Datos._
+import common._
 
+package object ItinerariosPar {
+  type Itinerario = List[Vuelo]
+
+  def convertirAHoraAbsoluta(hora: Int, minutos: Int, gmt: Int): Int = {
+    (hora + gmt) * 60 + minutos
+  }
+
+  def obtenerGMT(aeropuertos: List[Aeropuerto], codigo: String): Int = {
+    aeropuertos.find(_.Cod == codigo).map(_.GMT).getOrElse(0)
+  }
+
+  def convertirVuelosAHorasAbsolutas(vuelo: Vuelo, aeropuertos: List[Aeropuerto]): (Int, Int) = {
+    val gmtOrg = obtenerGMT(aeropuertos, vuelo.Org)
+    val gmtDst = obtenerGMT(aeropuertos, vuelo.Dst)
+    val horaSalida = convertirAHoraAbsoluta(vuelo.HS, vuelo.MS, gmtOrg)
+    val horaLlegada = convertirAHoraAbsoluta(vuelo.HL, vuelo.ML, gmtDst)
+    (horaSalida, horaLlegada)
+  }
+  def itinerarioValido(itinerario: Itinerario, aeropuertos: List[Aeropuerto]): Boolean = {
+    itinerario.zip(itinerario.tail).forall { case (vueloActual, vueloSiguiente) =>
+      val (_, llegadaActual) = convertirVuelosAHorasAbsolutas(vueloActual, aeropuertos)
+      val (salidaSiguiente, _) = convertirVuelosAHorasAbsolutas(vueloSiguiente, aeropuertos)
+      llegadaActual < salidaSiguiente
+    }
+  }
+
+  def itinerariosPar(vuelos: List[Vuelo], aeropuertos: List[Aeropuerto]): (String, String) => List[Itinerario] = {
+
+    def encontrarItinerarios(origen: String, destino: String, vuelosDisponibles: List[Vuelo], visitados: Set[String]): List[Itinerario] = {
+      if (origen == destino) {
+        return List(Nil)
+      }
+      (for {
+        vuelo <- vuelosDisponibles.filter(_.Org == origen)
+        subItinerarios = task(encontrarItinerarios(vuelo.Dst, destino, vuelosDisponibles, visitados + origen))
+        si = (subItinerarios.join).map(it => vuelo :: it)
+      }yield si).flatten
+    }
+
+    (cod1: String, cod2: String) => {
+      val posiblesItinerarios = encontrarItinerarios(cod1, cod2, vuelos, Set())
+      posiblesItinerarios.filter(itinerario => itinerarioValido(itinerario, aeropuertos))
+    }
+  }
 }
